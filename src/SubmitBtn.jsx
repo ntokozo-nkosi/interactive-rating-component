9b2036a6e9b1c524d553030f5cd3728ef641d505
ntokozo-nkosi <ntokozo.nkosi257@gmail.com>
2023-12-13 14:53:24 +0200

    Updates

--- src/SubmitBtn.jsx
+++
@@ -1,6 +0,0 @@
-import "./SubmitBtn.css"
-
-// eslint-disable-next-line react/prop-types
-export default function SubmitBtn({onSubmit}) {
-    return <button onClick={onSubmit} className="submit">Submit</button>
-}
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/interactive-rating-component/",
-  plugins: [react()],
-})
